9dd0f4b53d6c500961b5973bdbdf60f97844ee45
simomarsili <simomarsili@gmail.com>
2016-11-03 23:10:30 +0100

    rename project
    Former-commit-id: b9fd13757c8d97409b73a4d7b0bc38077d1b0759 [formerly 37c4a4bc09d2770564bf7142fcfd058b8d3a2f63]
    Former-commit-id: 7576f84c5b0d6611dc56131519377e14d9cd2a95

--- src/errors.f90
+++ src/errors.f90
@@ -18,25 +18,28 @@ module errors
   public :: dump_error
   character(len=1), parameter                  :: nl=achar(10)
   character(len=long_string_size)              :: syntax = nl//& 
-       '                                       elss (0.1.0)                                            '//nl//&
+       '                                       mcDCA (0.2.1)                                           '//nl//&
        '                                    ==================                                         '//nl//&
        '                                                                                               '//nl//&
-       ' "elss" is a Monte Carlo (MC) simulation code for the analysis of energy landscapes in protein '//nl//&
-       ' sequence spaces. It can either be used in conjunction with a user-defined energy function, or '//nl//&
-       ' to infer a data-driven statistical model from a multiple sequence alignment (MSA) via maximum '//nl//&
-       ' a posteriori (MAP) estimation[cite].'//nl//&
+       ' mcDCA  is a Monte Carlo (MC) code for the analysis and the inference of energy landscapes in  '//nl//&
+       ' protein sequence spaces. mcDAC can either be used to simulate a trajectory with a user-defined'//nl//&
+       ' energy function, or to infer a data-driven statistical model from a multiple sequence         '//nl//&
+       ' alignment (MSA) via maximum a posteriori (MAP) estimation. The energy function and its        '//nl//&
+       ' parameters control both the frequencies of amino acids at the different positions along the   '//nl//& 
+       ' chain and their correlations.                                                                 '//nl//& 
        nl//&
        nl//&
        'Option                         Description                                     (Default Value)  '//nl//&
        '------------------------------------------------------------------------------------------------'//nl//&
+       ' (-h|--help)                   print this help message                         (None)         '//nl//&
        nl//&
        ' (-p|--prm) <path_to_file>     parameters file                                 (None)           '//nl//&   
        '        OR'//nl//&   
-       ' --rst <path_to_file>          restart file                                    (None)           '//nl//&   
+       ' (-r|--rst) <path_to_file>     restart file                                    (None)           '//nl//&   
        nl//&
        ' (-n|--nsweeps) <int>          num. of MC sweeps                               (0)              '//nl//&
        nl//&
-       ' (-u|--nupdate) <int>          stride (as num. of sweeps) for updates          (10)             '//nl//&
+       ' (-u|--nupdate) <int>          stride (as num. of sweeps) for averages updates (10)             '//nl//&
        nl//&
        ' --fasta <path_to_file>        data file (MSA format)                          (None)           '//nl//&
        '        OR'//nl//&   
@@ -54,125 +57,113 @@ module errors
        nl//&
        ' (--learn|--learn-agd) <int>   num. of accelerated gradient descent steps      (0)              '//nl//&
        nl//&
-       ' (-l|--lambda) <float>         Gaussian prior (regularization) hyper-parameter (0.01)           '//nl//&
+       ' (-l|--lambda) <float>         (scaled) regularization parameter               (0.01)           '//nl//&
+       nl//&
+       ' --random_seed <int>           initialize the random seed                      (0)              '//nl//&
        '------------------------------------------------------------------------------------------------'//nl//&
        nl//&
        nl//&
-       'Usage Examples                                                                                  '//nl//&
        '------------------------------------------------------------------------------------------------'//nl//&
-       '                                                                                                    '//nl//&
-       ' elss --prm 1.prm  -n 1000                                                                          '//nl//&
-       '                                                                                                    '//nl//&
-       ' Description:                                                                                       '//nl//&    
-       ' simulate a (1000 sweeps) random walk in sequence space. Parameters for the energy function         '//nl//&
-       ' are read from file 1.prm.                                                                          '//nl//&
-       '                                                                                                    '//nl//&
-       ' mpiexec -n 16 elss --fasta msa.fa --agd 200 -gd 100 -n 10000                                       '//nl//&
-       '                                                                                                    '//nl//&    
-       ' Description:                                                                                       '//nl//&    
-       ' read data as a protein multiple sequence alignment in FASTA format, and infer the parameters of a  '//nl//&
-       ' fully visible and fully connected Boltzmann machine (a pairwise model of interacting amino acids). '//nl//&
-       ' The program will perform 200 steps of accelerated gradient descent followed by 100 steps of        '//nl//&
-       ' standard gradient descent algorithm.                                                               '//nl//&
-       ' An approximated regularized likelihood gradient is computed from the statistics of 16 independent  '//nl//&
-       ' persistent Markov chains (each consisting of 10000 MC sweeps).                                     '//nl//&
+       ' For more information and usage examples, please check the project github repository:           '//nl//&
+       ' https://github.com/simomarsili/mcDCA                                                           '//nl//&
+       '------------------------------------------------------------------------------------------------'//nl//&
        '                                                                                                    '
   character(len=string_size), dimension(-1:45) :: err_msg = & 
        [ &
        ! -1 print syntax only
-       '                                                                                         ', & 
+       '                                                                                          ', & 
        !  0 no errors
-       '                                                                                         ', & 
+       '                                                                                          ', & 
        !  1 
-       'elss: error                                                                              ', & 
+       'mcDCA: error                                                                              ', & 
        !  2
-       'elss: invalid option                                                                     ', & 
+       'mcDCA: invalid option                                                                     ', & 
        !  3
-       'elss: EVAL/MAP mode: please specify a data_file ([-i|--raw|--table|--fasta] <data_file>) ', & 
+       'mcDCA: EVAL/MAP mode: please specify a data_file ([-i|--raw|--table|--fasta] <data_file>) ', & 
        !  4
-       'elss: the [-i|--raw|--table|--fasta] option needs an argument <data_file>)               ', & 
+       'mcDCA: the [-i|--raw|--table|--fasta] option needs an argument <data_file>)               ', & 
        !  5
-       'elss: the [-w|--weights] option needs an argument <weights_file>)                        ', & 
+       'mcDCA: the [-w|--weights] option needs an argument <weights_file>)                        ', & 
        !  6
-       'elss: the [-r|--rst] option needs an argument <rst_file>)                                ', & 
+       'mcDCA: the [-r|--rst] option needs an argument <rst_file>)                                ', & 
        !  7
-       'elss: cannot access <data_file>: No such file or directory                               ', & 
+       'mcDCA: cannot access <data_file>: No such file or directory                               ', & 
        !  8
-       'elss: cannot access <weights_file>: No such file or directory                            ', & 
+       'mcDCA: cannot access <weights_file>: No such file or directory                            ', & 
        !  9 
-       'elss: cannot access <rst_file>: No such file or directory                                ', & 
+       'mcDCA: cannot access <rst_file>: No such file or directory                                ', & 
        !  10
-       'elss: please check nsweeps value ([-n|--nsweeps] <nsweeps>)                              ', & 
+       'mcDCA: please check nsweeps value ([-n|--nsweeps] <nsweeps>)                              ', & 
        !  11
-       'elss: please check niter value ([--agd] <niter>)                                         ', & 
+       'mcDCA: please check niter value ([--agd] <niter>)                                         ', & 
        !  12
-       'elss: please check niter value ([--gd] <niter>)                                          ', & 
+       'mcDCA: please check niter value ([--gd] <niter>)                                          ', & 
        !  13
-       'elss: please check wid value (--wid <wid>)                                               ', & 
+       'mcDCA: please check wid value (--wid <wid>)                                               ', & 
        !  14 
-       'elss: you must use either the [-w|--weights] or the --wid option                         ', & 
+       'mcDCA: you must use either the [-w|--weights] or the --wid option                         ', & 
        !  15 
-       'elss: please check <nupdate> value ([-u|--nupdate] <nupdate>)                            ', & 
+       'mcDCA: please check <nupdate> value ([-u|--nupdate] <nupdate>)                            ', & 
        !  16 
-       'elss: you must specify a single data_file ([-i|--raw|--table|--fasta] <data_file>)       ', & 
+       'mcDCA: you must specify a single data_file ([-i|--raw|--table|--fasta] <data_file>)       ', & 
        !  17 
-       'elss: you must specify a single weights_file ([-w|--weights] <weights_file>)             ', & 
+       'mcDCA: you must specify a single weights_file ([-w|--weights] <weights_file>)             ', & 
        !  18 
-       'elss: you must specify a single rst_file ([-r|--rst] <rst_file>)                         ', & 
+       'mcDCA: you must specify a single rst_file ([-r|--rst] <rst_file>)                         ', & 
        !  19 
-       'elss: error opening file                                                                 ', & 
+       'mcDCA: error opening file                                                                 ', & 
        !  20 
-       'elss: error reading data_file                                                            ', & 
+       'mcDCA: error reading data_file                                                            ', & 
        !  21 
-       'elss: error reading weights_file                                                         ', & 
+       'mcDCA: error reading weights_file                                                         ', & 
        !  22 
-       'elss: error reading rst_file                                                             ', & 
+       'mcDCA: error reading rst_file                                                             ', & 
        !  23 
-       'elss: data_file and weights file must have the same number of lines                      ', & 
+       'mcDCA: data_file and weights file must have the same number of lines                      ', & 
        !  24 
-       'elss: unkown amino acid                                                                  ',  & 
+       'mcDCA: unkown amino acid                                                                  ',  & 
        !  25 
-       'elss: in rst file, nvars is not consistent with data                                     ',  & 
+       'mcDCA: in rst file, nvars is not consistent with data                                     ',  & 
        !  26 
-       'elss: you must specify a single prm_file ([-p|--prm] <prm_file>)                         ', & 
+       'mcDCA: you must specify a single prm_file ([-p|--prm] <prm_file>)                         ', & 
        !  27
-       'elss: the [-p|--prm] option needs an argument <prm_file>)                                ', & 
+       'mcDCA: the [-p|--prm] option needs an argument <prm_file>)                                ', & 
        !  28 
-       'elss: cannot access <prm_file>: No such file or directory                                ', & 
+       'mcDCA: cannot access <prm_file>: No such file or directory                                ', & 
        !  29 
-       'elss: error reading fields in <prm_file>                                                 ', & 
+       'mcDCA: error reading fields in <prm_file>                                                 ', & 
        !  30 
-       'elss: error reading couplings in <prm_file>                                              ', & 
+       'mcDCA: error reading couplings in <prm_file>                                              ', & 
        !  31 
-       'elss: unexpected number of fields in <prm_file>                                          ', & 
+       'mcDCA: unexpected number of fields in <prm_file>                                          ', & 
        !  32 
-       'elss: error opening array file                                                           ', & 
+       'mcDCA: error opening array file                                                           ', & 
        !  33 
-       'elss: unexpcted memory allocation                                                        ', & 
+       'mcDCA: unexpcted memory allocation                                                        ', & 
        !  34 
-       'elss: error opening rst file                                                             ', & 
+       'mcDCA: error opening rst file                                                             ', & 
        !  35 
-       'elss: rst and prm reads cannot go together                                               ', & 
+       'mcDCA: rst and prm reads cannot go together                                               ', & 
        !  36 
-       'elss: SIM/EVAL modes require a rst file or a prm file                                    ', & 
+       'mcDCA: SIM/EVAL modes require a rst file or a prm file                                    ', & 
        !  37 
-       'elss: MAP mode: please specify a nsweeps value ([-n|--nsweeps] <nsweeps>)                ', & 
+       'mcDCA: MAP mode: please specify a nsweeps value ([-n|--nsweeps] <nsweeps>)                ', & 
        !  38 
-       'elss: MAP mode: lambda hyperparameter cannot be negative ([-l|--lambda] <lamnda>)        ', & 
+       'mcDCA: MAP mode: lambda hyperparameter cannot be negative ([-l|--lambda] <lamnda>)        ', & 
        !  39 
-       'elss: in prm file: in protein, num. of classes must be 21 ([-p|--prm] <prm_file>)        ', & 
+       'mcDCA: in prm file: in protein, num. of classes must be 21 ([-p|--prm] <prm_file>)        ', & 
        !  40 
-       'elss: you must specify a single seq_file ([-s|--seq] <seq_file>)                         ', & 
+       'mcDCA: you must specify a single seq_file ([-s|--seq] <seq_file>)                         ', & 
        !  41
-       'elss: the [-s|--seq] option needs an argument <seq_file>)                                ', & 
+       'mcDCA: the [-s|--seq] option needs an argument <seq_file>)                                ', & 
        !  42 
-       'elss: cannot access <seq_file>: No such file or directory                                ', & 
+       'mcDCA: cannot access <seq_file>: No such file or directory                                ', & 
        !  43 
-       'elss: not a valid fasta file                                                             ', & 
+       'mcDCA: not a valid fasta file                                                             ', & 
        !  44
-       'elss: please check random seed value (--random_seed <rseed>)                             ', & 
+       'mcDCA: please check random seed value (--random_seed <rseed>)                             ', & 
        !  45
-       'elss: check temperature factor [-t|--temp] <temp>)                                       ' & 
+       'mcDCA: check temperature factor [-t|--temp] <temp>)                                       ' & 
        ]
 
 contains
