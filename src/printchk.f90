c696289b68577797d5da3703309a5498c8125ed0
simomarsili <simomarsili@gmail.com>
2017-02-10 18:21:13 +0100

    removed errors.f90 from Makefile; added printchk
    Former-commit-id: e6e18b08f8ebe64acca3f614e1ea8e207b6f11a7 [formerly a0ce919fca1e495366b952b1574cc9f991df81c2]
    Former-commit-id: 551f2efc02db43495ba376e47a5d795fb291236c [formerly 4ad25c97843398885394f5152155e82375bd141f]
    Former-commit-id: 7be77b0a33105cb7a34e07a089d599b44aab16b3

---
+++ src/printchk.f90
@@ -0,0 +1,86 @@
+! Copyright (C) 2016, Simone Marsili 
+! All rights reserved.
+! License: BSD 3 clause
+
+program printchk
+  use kinds
+  use constants
+  use units, only: units_open_unf
+  use arguments, only: read_opt,read_arg
+  
+  integer                    :: unt=11
+  character(len=string_size) :: data_format
+  integer                    :: nvars,nclasses
+  integer,       allocatable :: seq(:,:)
+  real(kflt),    allocatable :: prm(:)
+  integer                    :: p,np,err
+  integer                    :: nv,nc
+  character(len=long_string_size) :: cmd
+  integer                         :: nargs
+  character(len=long_string_size) :: arg
+  integer                         :: iarg
+  logical                         :: file_exists
+  character(len=long_string_size) :: syntax = 'syntax'
+  character(len=long_string_size) :: rst_file
+  
+  ! read arguments
+  call get_command(cmd)
+  nargs = command_argument_count()
+  
+  ! call with no args
+  if (nargs == 0) then
+     stop
+  end if
+
+  iarg = 0
+  args_loop: do while(iarg < nargs)
+     call read_opt(iarg,nargs,arg,err)
+     select case(trim(arg))
+     case('-h','--help')
+        ! print help and exit
+        write(0,*) trim(syntax)
+        stop
+     case('-i')
+        ! prm file
+        call read_arg(iarg,nargs,rst_file,err)
+        if (err == 1) then
+           write(0,*) 'ERROR ! missing argument: '//trim(arg)//' <filename>'
+           stop
+        end if
+     case default
+        write(0,*) 'ERROR ! unkown argument '//trim(arg)
+        stop
+     end select
+  end do args_loop
+
+  call units_open_unf(rst_file,'old',unt,err)
+  
+  ! read binary file
+  read(unt) data_format
+  read(unt) nvars
+  read(unt) nclasses
+  read(unt) np
+  allocate(prm(nvars*nclasses + nvars*(nvars - 1)*nclasses**2/2),stat=err)
+  if (np > 0) then
+     ! seqs found in restart
+     allocate(seq(nvars,np),stat=err)
+     seq = 0
+     do p = 1,np
+        read(unt) seq(:,p)
+     end do
+  end if
+  prm = 0.0_kflt
+  read(unt) prm
+
+  ! dump file
+  write(*,'(a)') '# format nv nc np'
+  write(*,'(a,3(1x,i4))') trim(data_format), nvars, nclasses, np
+  if (np > 0) then
+     do p = 1,np
+        write(*,'(a,1000i3)') '# ',seq(:,p)
+     end do
+  end if
+  write(*,*) prm(:10)
+  stop
+
+end program printchk
