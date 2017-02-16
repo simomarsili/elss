904b63b86f87d57a73c7f45ec25077352622ec26
simomarsili <simomarsili@gmail.com>
2017-02-16 18:15:54 +0100

    added datatype
    Former-commit-id: 05ebcf3cf989f926dbc6423ded27d3e89b5c21b3 [formerly ec468209cdfb9b9e041b7b42deedb6c0e6ed328f]
    Former-commit-id: 907ccabd66fd8dd8a0d1a966a56bc604a75e3d0c [formerly 03b6467ee8a2126de5310bd157998f487eb3a6c4]
    Former-commit-id: f596a23fbd30b0ef911ad773d5af44e69bd59b27

--- src/fasta.f90
+++ src/fasta.f90
@@ -7,28 +7,56 @@ module fasta
   implicit none
   private 
   public :: protein_alphabet
-  public :: fasta_init
   public :: fasta_read
-  public :: fasta_string2seq
   character(len=1) :: protein_alphabet(21) = &
        ['A','C','D','E','F','G','H','I','K',&
        'L','M','N','P','Q','R','S','T','V','W','Y','-']
   character(len=1) :: nuc_acid_alphabet(6) = &
        ['A','C','G','T','U','-']
+  character(len=21) :: protein_set='ACDEFGHIKLMNPQRSTVWY-'
+  character(len=5)  :: nuc_acid_set='ACGTU-'
   integer          :: aamap(239) ! 239 is n. of ascii codes (0 is null)
 
 contains
 
-  subroutine fasta_init()
+  subroutine fasta_init(data_type)
+    character(len=*), intent(in) :: data_type
+    ! set the aa -> class mapping
     integer :: k,nn
     
     aamap = 0
-    do k = 1,21
-       nn = iachar(protein_alphabet(k))
-       aamap(nn) = k
-    end do
+    select case(data_type)
+    case('protein')
+       do k = 1,21
+          nn = iachar(protein_alphabet(k))
+          aamap(nn) = k
+       end do
+    case('nuc_acid')
+       do k = 1,6
+          nn = iachar(nuc_acid_alphabet(k))
+          aamap(nn) = k
+       end do
+    end select
     
   end subroutine fasta_init
+
+  function is_nuc_acid(string) result(res)
+    character(len=*), intent(in)  :: string
+    logical :: res
+
+    res = .false.
+    if (verify(string,nuc_acid_set) == 0) res = .true.
+    
+  end function is_nuc_acid
+  
+  function is_protein(string) result(res)
+    character(len=*), intent(in)  :: string
+    logical :: res
+
+    res = .false.
+    if (verify(string,protein_set) == 0) res = .true.
+    
+  end function is_protein
 
   function aa_to_class(aa) result(c)
     ! takes an a.a. as input, return its index
@@ -63,30 +91,41 @@ contains
     
   end subroutine fasta_string2seq
 
-  subroutine fasta_read(unt,seqs,error_code,error_string)
+  subroutine fasta_read(unt,seqs,data_type,error_code,error_string)
     ! read n sequences from unit udata
     integer,              intent(in)  :: unt
     integer, allocatable, intent(out) :: seqs(:,:)
+    character(len=*),     intent(out) :: data_type
     integer,              intent(out) :: error_code
     character(len=*),     intent(out) :: error_string
     character(len=long_string_size) :: header
     character(len=long_string_size) :: string
     character(len=long_string_size) :: line
     integer, allocatable :: seq(:)
-    integer                         :: k,nn,nv
+    integer                         :: k,ns,nv,nnuc,nprot
     integer                         :: err
 
-    call fasta_init()
-
-    ! simply count seqs
-    nn = 0
+    ! count seqs and Ls
+    ns = 0
+    nnuc = 0
+    nprot = 0
     do
        read(unt,'(a)',iostat=err) line
        ! exit at end of file or empty line
        if ( err < 0 .or. len_trim(line) == 0 ) exit
-       if ( line(1:1) == ">" ) nn = nn + 1
+       if ( line(1:1) == ">" ) then
+          ns = ns + 1
+       else
+          if (is_nuc_acid(line)) nnuc = nnuc + 1
+          if (is_protein(line)) nprot = nprot + 1
+       end if
     end do
     rewind(unt)
+
+    data_type = 'protein'
+    if (nnuc > nprot) data_type = 'nuc_acid'
+
+    call fasta_init(data_type)
 
     string = ""
     k = 1
@@ -100,7 +139,7 @@ contains
           if (k == 1) then 
              nv = len_trim(string)
              allocate(seq(nv),stat=err)
-             allocate(seqs(nv,nn),stat=err)
+             allocate(seqs(nv,ns),stat=err)
           end if
           call fasta_string2seq(string,seq,error_code,error_string)
           if (error_code > 0) return
@@ -112,7 +151,7 @@ contains
           if (k == 1) then 
              nv = len_trim(string)
              allocate(seq(nv),stat=err)
-             allocate(seqs(nv,nn),stat=err)
+             allocate(seqs(nv,ns),stat=err)
           end if
           call fasta_string2seq(string,seq,error_code,error_string)
           if (error_code > 0) return
