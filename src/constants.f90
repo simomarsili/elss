304653308f9e0c274ca030bfac55695eb83a747d
simomarsili <simomarsili@gmail.com>
2017-10-24 16:01:15 +0200

    short_string_size = 10
    Former-commit-id: 3d0a16a6d73b5003ce4aa9d138fea59162a4e2e3 [formerly 60fc4d5628cc719c89f9272b6d6f922dde56c6ce]
    Former-commit-id: 034a0eb40bcba983c8238e2bc0e02fe6ca4927ca

--- src/constants.f90
+++ src/constants.f90
@@ -7,6 +7,6 @@ module constants
 
   integer, parameter :: long_string_size = 100000
   integer, parameter :: string_size = 1000
-  integer, parameter :: short_string_size = 5
+  integer, parameter :: short_string_size = 10
 
 end module constants
