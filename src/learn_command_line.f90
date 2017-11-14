fb349532a0f5f4e0f4cef35e9dbf857c638227e2
simomarsili <simomarsili@gmail.com>
2017-11-14 16:15:49 +0100

    bump to version 0.4
    Former-commit-id: b6aeb3fefb8fbe8ce83fdd9b869b8e4ca8dec690 [formerly 5482a04f3660f70992ac252b16d7e0936a3f6a42]
    Former-commit-id: 654d93d8f8507d3392724c22b0454d0417ac4757 [formerly dbe81329b461b528f6495acdb29c38962620455a]
    Former-commit-id: 89b3a3edfacc46980b6ef836539fcdf0866f6038

--- src/learn_command_line.f90
+++ src/learn_command_line.f90
@@ -281,7 +281,7 @@ contains
     end if
 
 100 format(&
-         'elss-learn (elss v0.3.2)                                            '/& 
+         'elss-learn (elss v0.4)                                            '/& 
          '                                                                  '/&
          'Usage:                                                            '/&
          '    elss-learn [options] (-i <file> | --fasta <file>)             '/&
