cc3c902396dcc13bab047b83b391aa8094740201
simomarsili <simomarsili@gmail.com>
2017-11-17 17:32:38 +0100

    add update_single, update_pair; use update_counts in data.f90

--- src/averages.f90
+++ src/averages.f90
@@ -6,15 +6,52 @@ module averages
   use kinds
   implicit none
   private
-  public :: update_counts
+  public :: update_single, update_pair, update_counts
 
 contains
 
-  subroutine update_counts(w, seq, freq_single, freq_pair)
+  subroutine update_single(w, seq, single)
     real(kflt), intent(in)    :: w
     integer,    intent(in)    :: seq(:)
-    real(kflt), intent(inout) :: freq_single(:, :)
-    real(kflt), intent(inout) :: freq_pair(:, :, :)
+    real(kflt), intent(inout) :: single(:, :)
+    integer :: nv, iv, is
+
+    nv = size(seq)
+
+    do iv = 1, nv
+       is = seq(iv)
+       single(is, iv) = single(is, iv) + w
+    end do
+
+  end subroutine update_single
+
+  subroutine update_pair(w, seq, pair)
+    real(kflt), intent(in)    :: w
+    integer,    intent(in)    :: seq(:)
+    real(kflt), intent(inout) :: pair(:, :, :)
+    integer :: iv, jv
+    integer :: is, js
+    integer :: k, nv
+
+    nv = size(seq)
+
+    k = 0
+    do jv = 1, nv-1
+       js = seq(jv)
+       do iv = jv+1, nv
+          is = seq(iv)
+          k = k + 1
+          pair(is, js, k) = pair(is, js, k) + w
+       end do
+    end do
+
+  end subroutine update_pair
+
+  subroutine update_counts(w, seq, single, pair)
+    real(kflt), intent(in)    :: w
+    integer,    intent(in)    :: seq(:)
+    real(kflt), intent(inout) :: single(:, :)
+    real(kflt), intent(inout) :: pair(:, :, :)
     integer :: iv, jv
     integer :: is, js
     integer :: k, nv
@@ -23,7 +60,7 @@ contains
 
     do iv = 1, nv
        is = seq(iv)
-       freq_single(is, iv) = freq_single(is, iv) + w
+       single(is, iv) = single(is, iv) + w
     end do
 
     k = 0
@@ -32,7 +69,7 @@ contains
        do iv = jv+1, nv
           is = seq(iv)
           k = k + 1
-          freq_pair(is, js, k) = freq_pair(is, js, k) + w
+          pair(is, js, k) = pair(is, js, k) + w
        end do
     end do
 
